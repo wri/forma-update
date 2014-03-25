9fa5d1ac9be860039b8160b3adf357a63f8674b1
Robin Kraft <rkraft4@gmail.com>
2014-03-24 22:00:43 -0700

    WIP

--- formaupdate/runner.py
+++ formaupdate/runner.py
@@ -8,10 +8,10 @@ import requests
 from utils import *
 
 # table containing data in common data model
-INITTABLE = "cdm_latest"
+INITTABLE = "cdm_2014_03_16"
 
 # temporary table during update process
-TABLE = "gfw2_forma_ew"
+TABLE = "gfw2_forma_ew1"
 
 # table for checking whether specific pixels were correctly processed
 CONTROLTABLE = 'gfw2_forma_control_pixels'
@@ -22,7 +22,8 @@ APIKEY = os.environ["CARTODB_API_KEY"]
 BASEURL = "https://wri-01.cartodb.com/api/v2/sql?api_key=%s&q=" % APIKEY
 
 RANGEFIELD = 'cartodb_id'
-MAXZOOM = 17
+RANGEFIELD2 = 'cartodb_id'
+MAXZOOM = 15
 MINZOOM = 6
 STEPCOUNT = 20
 
@@ -45,6 +46,7 @@ UPDATENULLSE17 = range_query(MAXZOOM, MAXZOOM + 1, UPDATENULLSE + " WHERE se is 
 DROPINDEX = "DROP INDEX IF EXISTS %s_%s_idx"
 CREATEINDEX = "CREATE INDEX %s_%s_idx ON %s (%s)"
 
+
 def create_indexes(drop_query, create_query, table, base_url):
 
     queries = gen_drop_index_queries(drop_query, table)
@@ -55,15 +57,14 @@ def create_indexes(drop_query, create_query, table, base_url):
 def run_z17(base_url, step_count, init_table, table, z, zoom_sub,
             zoom, sd_query, se_query, range_field, ctrl_table):
 
-    results = []
-
     # get range for init table
     minid, maxid, stepsize = calc_range_params(base_url, step_count,
                                                init_table, range_field)
-
     # gen queries to load data into new table for z17
     queries = gen_load_17_query(zoom_sub, zoom, init_table, table,
                                 minid, maxid, stepsize, z, range_field)
+
+    results = []
 
     # run queries
     results = run_queries(table, base_url, queries)
@@ -88,8 +89,7 @@ def run_z17(base_url, step_count, init_table, table, z, zoom_sub,
     if zoom_ok(z, table, ctrl_table, base_url):
         return results
 
-    else:
-        raise Exception("Zoom error: %d" % z)
+    return results
 
 def process_zoom(table, z, base_url, step_count, zoom_sub, zoom,
                  update_sd, update_se, range_field, ctrl_table):
@@ -105,7 +105,7 @@ def process_zoom(table, z, base_url, step_count, zoom_sub, zoom,
     # gen queries to load data for zoom level
     queries = gen_load_Z_query(zoom_sub, zoom, table, minid, maxid,
                                stepsize, z, range_field)
-
+    
     results += run_queries(table, base_url, queries)
 
     # bulk load add data - works better ...
@@ -132,9 +132,6 @@ def process_zoom(table, z, base_url, step_count, zoom_sub, zoom,
     if zoom_ok(z, table, ctrl_table, base_url):
         return results
 
-    else:
-        raise Exception("Zoom error: %d" % z)
-
 def main(input_table=INITTABLE, output_table=TABLE, z_min=MINZOOM, z_max=MAXZOOM):
     t = time.time()
     responses = []
@@ -147,11 +144,11 @@ def main(input_table=INITTABLE, output_table=TABLE, z_min=MINZOOM, z_max=MAXZOOM
     # create indexes for table
     responses += create_indexes(DROPINDEX, CREATEINDEX, TABLE, BASEURL)
 
-    # process data for zooms below 17
-    for z in range(z_max - 1, z_min - 1, -1): # z17 already done
-        process_zoom(TABLE, z, BASEURL, STEPCOUNT, ZOOMSUBQUERY,
-                     ZOOMQUERY, UPDATENULLSD, UPDATENULLSE,
-                     RANGEFIELD, CONTROLTABLE)
+    # # process data for zooms below 17
+    # for z in range(z_max - 1, z_min - 1, -1): # z17 already done
+    #     process_zoom(TABLE, z, BASEURL, STEPCOUNT, ZOOMSUBQUERY,
+    #                  ZOOMQUERY, UPDATENULLSD, UPDATENULLSE,
+    #                  RANGEFIELD2, CONTROLTABLE)
 
     print "\nElapsed time: %0.1f minutes" % ((time.time() - t) / 60)
     return responses
